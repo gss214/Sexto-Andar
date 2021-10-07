8c147b585a3123ca49f8b5158ba04ed6cab08165
Guilherme Silva Souza <gss214@hotmail.com>
2021-10-07 12:45:47 -0300

    html pronto para dar update no perfil

--- app/controllers/crud.py
+++ app/controllers/crud.py
@@ -1,5 +1,6 @@
 from app import app, cnx
-from flask import render_template, session
+from flask import request, render_template, session
+from werkzeug.utils import redirect
 from app.models.cliente_dao import ClienteDAO
 from app.models.login_dao import LoginDAO
 from app.models.endereco_dao import EnderecoDAO
@@ -8,19 +9,27 @@ from app.models.endereco_dao import EnderecoDAO
 def crud():
     return render_template("crud.html")
 
-@app.route("/perfil")
+def editar_endereco():
+    pass
+
+@app.route('/perfil', methods=['GET', 'POST'])
 def perfil():
 
-    cursor = cnx.connection.cursor()
+    if request.method == 'GET':
+        cursor = cnx.connection.cursor()
 
-    login = LoginDAO().find_by_id(cursor, session['user_id'])
-    login = [login.email, login.senha]
+        login = LoginDAO().find_by_id(cursor, session['user_id'])
+        login = [login.email, login.senha]
 
-    sql = f"SELECT * FROM cliente WHERE fk_login = '{session['user_id']}'"
-    cursor.execute(sql)
-    cliente = cursor.fetchone()
+        sql = f"SELECT * FROM cliente WHERE fk_login = '{session['user_id']}'"
+        cursor.execute(sql)
+        cliente = cursor.fetchone()
 
-    endereco = EnderecoDAO().find_by_id(cursor, cliente[4])
-    endereco = [endereco.CEP, endereco.rua, endereco.bairro, endereco.cidade, endereco.estado, endereco.numero, endereco.complemento]
+        endereco = EnderecoDAO().find_by_id(cursor, cliente[4])
+        endereco = [endereco.CEP, endereco.rua, endereco.bairro, endereco.cidade, endereco.estado, endereco.numero, endereco.complemento]
 
-    return render_template("perfil.html", cliente=cliente, login=login, endereco=endereco)
+        return render_template("perfil.html", cliente=cliente, login=login, endereco=endereco)
+    else:
+        if 'salvarPefil' in request.form:
+            print('update perfil')
+        return redirect('/perfil')
