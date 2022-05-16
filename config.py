c5649ac7a842caade9b004fdd3a0f35c5f1d74ff
Guilherme Souza <gss214@hotmail.com>
2022-05-16 00:57:14 -0300

    ajustando o host do db e alterando o readme

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'root'
-    MYSQL_HOST = 'db'
+    MYSQL_HOST = 'sexto_andar_db'
     MYSQL_DB = 'sextoandar'
 
 config = {
